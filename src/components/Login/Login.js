9b71b73aa59697e15e86ad29c14b4c05146e918d
KyZerark <neftvc@hotmail.com>
2023-02-22 22:19:32 -0600

    Modificaciones menores de los formularios

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,26 +7,34 @@ import lock from '../icons/lock.png';
 function Login(){
     return(
         <>
-            <div className="login-component">
-                <h2>&lt;Keisoline&gt;</h2>
-                <div className="login-formulario">
-                    <form>
-                        <div className="campo">
-                            <img src={usuario} alt="user-icon"></img>
-                            <input type="text" name="correo"/>
-                        </div>
-                        <div className="campo">
+        <div className="login-component">
+            <header>
+                <h1>&lt;Keisoline&gt;</h1>
+            </header>
+            
+            <div className="login-formulario">
+                <form>
+                    <div className="campo">
+                        <img src={usuario} alt="user-icon"></img>
+                        <input type="text" name="correo"/>
+                    </div>
+                    <div className="campo">
                         <img src={lock} alt="lock-icon"></img>
                         <input type="password" name="password"/>
-                        </div>
-                        <input className="boton-azul" type="submit" value="Iniciar sesión"></input>
-                        <Link to="#">Recuperar contraseña</Link>
-                    </form>
-                    
-                    <hr/>
-                    <button className="boton-negro">Registrarse</button>
-                </div>
+                    </div>
+                    <input className="boton-azul" type="submit" value="Iniciar sesión"></input>
+                    <Link to="#">Recuperar contraseña</Link>
+                </form>
+                
+                <div className="rlinea"> <hr/> </div>
+
+                <button className="boton-negro">Registrarse</button>
             </div>
+
+            <footer>
+                © 2023 | Instituto Politécnico Nacional - Escuela Superior de Cómputo
+            </footer>
+        </div>
         </>
     );
 }
